ccf16ea1883f1566a1b3a97b1c3527aa8f70f4fe
Alex Klyanchin <jetblackmeerkat@gmail.com>
2020-05-10 14:18:31 +0300

    Improve style

--- k-nearest_neighbors/knn.py
+++ k-nearest_neighbors/knn.py
@@ -1,13 +1,17 @@
-import pandas as pd
+"""Simple KNN implementation"""
+
 from math import sqrt
 from collections import Counter
 from itertools import chain
+import pandas as pd
 
 
 def load_dataset(file):
+    """Loads dataset from file and prints a sample"""
+
     dataset = pd.read_csv(file).to_numpy().tolist()
 
-    print(f"Dataset sample:")
+    print("Dataset sample:")
     print(*dataset[:5], sep="\n")
     print()
 
@@ -15,6 +19,8 @@ def load_dataset(file):
 
 
 def euclidean_distance(vector1, vector2):
+    """Calculates Euclidean distance for two vectors"""
+
     if len(vector1) != len(vector2):
         raise ValueError(
             f"Vectors must be of the same length. Got {vector1} and {vector2}."
@@ -30,6 +36,8 @@ def euclidean_distance(vector1, vector2):
 
 
 def split_in_folds(data, folds_number):
+    """Split uniform datasets to specified number of groups"""
+
     folds = list()
     data_size = len(data)
     fold_size = data_size // folds_number + 1
@@ -42,7 +50,15 @@ def split_in_folds(data, folds_number):
     return folds
 
 
+def flatten(list_of_lists):
+    """Flattens a list of lists"""
+
+    return list(chain.from_iterable(list_of_lists))
+
+
 def cross_validation_sets(folds):
+    """Makes sets for cross validation from specified list of groups"""
+
     sets = list()
 
     for fold in folds:
@@ -58,6 +74,8 @@ def cross_validation_sets(folds):
 
 
 def cross_validation_split(data, folds_number):
+    """Produces sets for crossvalidation from dataset"""
+
     folds = split_in_folds(data, folds_number)
     sets = cross_validation_sets(folds)
 
@@ -65,6 +83,8 @@ def cross_validation_split(data, folds_number):
 
 
 def knn(train, test, k):
+    """Calcualtes specified number of nearest neighbors"""
+
     distances = [(row, euclidean_distance(row[:-1], test)) for row in train]
     distances.sort(key=lambda distance: distance[1])
 
@@ -72,6 +92,8 @@ def knn(train, test, k):
 
 
 def predict_class(train, test, k):
+    """Predicts class for test data based on it's neighbors"""
+
     neighbors = knn(train, test, k)
     classes = [neighbor[0][-1] for neighbor in neighbors]
 
@@ -80,16 +102,12 @@ def predict_class(train, test, k):
     return prediction
 
 
-def flatten(list_of_lists):
-    return list(chain.from_iterable(list_of_lists))
-
-
-train = load_dataset('cats_dataset.csv')
-test = [27, 51, 3.7]
+train_set = load_dataset('cats_dataset.csv')
+test_object = [27, 51, 3.7]
 number_of_neighbors = 20
 
-print(f"Classifying a cat with height, length and weight {test}")
+print(f"Classifying a cat with height, length and weight {test_object}")
 print()
 
-prediction = predict_class(train, test, number_of_neighbors)
-print(f"Breed prediction for the cat is {prediction}")
+predicted_class = predict_class(train_set, test_object, number_of_neighbors)
+print(f"Breed prediction for the cat is {predicted_class}")
